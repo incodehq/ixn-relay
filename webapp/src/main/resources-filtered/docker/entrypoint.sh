fef534becd696648d91f71cd8fe34a09e769fcc5
danhaywood <dan@haywood-associates.co.uk>
2019-10-04 10:11:56 +0100

    ECP-1053: adds configuration to build dockerfile

---
+++ webapp/src/main/resources-filtered/docker/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#
+# Docker swarm will mount any secrets into this directory
+#
+# The idea is to just define application.yaml as the secret
+# As per https://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-external-config.html#boot-features-external-config-application-property-files,
+# this should be picked up automatically.
+#
+if [ -d /run/secrets ]
+then
+  cd /run/secrets || exit 1
+fi
+
+java -Djava.security.egd=file:/dev/./urandom -jar /app.jar
+
