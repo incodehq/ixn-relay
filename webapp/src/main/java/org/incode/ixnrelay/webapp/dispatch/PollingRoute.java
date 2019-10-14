e64837c663a2c7deef1115ed36d70ce8852d4ed0
danhaywood <dan@haywood-associates.co.uk>
2019-10-14 14:20:02 +0100

    adds in manual zipkin support

--- webapp/src/main/java/org/incode/ixnrelay/webapp/dispatch/PollingRoute.java
+++ webapp/src/main/java/org/incode/ixnrelay/webapp/dispatch/PollingRoute.java
@@ -1,6 +1,7 @@
 package org.incode.ixnrelay.webapp.dispatch;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.zipkin.ZipkinTracer;
 import org.incode.ixnrelay.webapp.config.AppConfig;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,22 @@ public class PollingRoute extends RouteBuilder {
 
     @Override
     public void configure() {
+
+        switch (appConfig.getTracing().getTracingType()) {
+            case ZIPKIN:
+                ZipkinTracer zipkin = new ZipkinTracer();
+                AppConfig.Tracing.Zipkin zipkinConfig = appConfig.getTracing().getZipkin();
+                zipkin.setEndpoint(zipkinConfig.getEndpoint());
+                zipkin.setServiceName(zipkinConfig.getServiceName());
+                zipkin.setRate(zipkinConfig.getRate());
+                zipkin.setIncludeMessageBodyStreams(zipkinConfig.isIncludeMessageBodyStreams());
+
+
+                // add zipkin to CamelContext
+                zipkin.init(getContext());
+                break;
+        }
+
         from("timer:poll?period={{app.polling-period}}")
             .to("direct:poll");
     }
